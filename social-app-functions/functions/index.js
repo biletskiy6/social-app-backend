b58bf13f8ce00d9fdf89c2a0fd696daf95baa477
Victor Biletsky <victor.biletsky@tangramltd.com>
2019-10-17 17:53:11 +0300

    add cors

--- social-app-functions/functions/index.js
+++ social-app-functions/functions/index.js
@@ -1,8 +1,12 @@
 const functions = require("firebase-functions");
 
 const app = require("express")();
-
 const FBAuth = require("./utils/fbAuth");
+
+
+const cors = require('cors');
+app.use(cors());
+
 
 const { db } = require('./utils/admin');
 
